844214f526ca1b1fb64add72cfbf830cdf8b9310
Masahiro Nakagawa <repeatedly@gmail.com>
2013-04-25 15:01:16 -0700

    Remove testing code

--- lib/hadoop_metrics.rb
+++ lib/hadoop_metrics.rb
@@ -1,13 +1,2 @@
 require 'hadoop_metrics/job_tracker'
 require 'hadoop_metrics/task_tracker'
-
-jt = HadoopMetrics::JobTracker.new('localhost', 50030, :snake_case => false)
-#puts JSON.pretty_generate(jt.fairscheduler_jobs)
-#puts JSON.pretty_generate(jt.mapred_jobtracker('hostName'))
-#puts JSON.pretty_generate(jt.fairscheduler_pools)
-puts JSON.pretty_generate(jt.fairscheduler_running_tasks)
-#puts JSON.pretty_generate(jt.jmx)
-
-#tt = HadoopMetrics::TaskTracker.new('localhost', 50060, :snake_case => false)
-#puts JSON.pretty_generate(tt.shuffle_output)
-#puts JSON.pretty_generate(tt.mapred)
